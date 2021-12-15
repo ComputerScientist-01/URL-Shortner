124938e8167aef7741bda85d371960d680b141e1
ComputerScientist-01 <rathi406@gmail.com>
2021-12-15 18:35:38 +0530

    🗃️ import connectDB with dotenv

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const app = express();
 app.listen(process.env.PORT || 5000);
+const connectDB = require("./db/connect");
 
+require("dotenv").config();
 app.set("view engine", "ejs");
+
+app.post("/shortUrls", function (req, res) {
+	res.send("POST request to the homepage");
+});
 
 app.get("/", (req, res) => {
 	res.render("index");
 });
+
+//const mySchema = new Schema({
+// _id: {
+// 	type: String,
+// 	default: () => nanoid()
+//   }
+// })
