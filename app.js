74445e2c251eec76b71dfffc92a948e1a3721bcd
ComputerScientist-01 <rathi406@gmail.com>
2022-01-14 16:57:14 +0530

    🗃️ add db connect f(x)

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const app = express();
-app.listen(process.env.PORT || 5000);
 const connectDB = require("./db/connect");
 
 require("dotenv").config();
 app.set("view engine", "ejs");
+
+const port = 3000;
+
+const start = async () => {
+	try {
+		await connectDB(process.env.MONGO_URL);
+		app.listen(port, process.stdout.write(`Server is listening on ${port}...`));
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+start();
 
 app.post("/shortUrls", function (req, res) {
 	res.send("POST request to the homepage");
@@ -13,10 +25,3 @@ app.post("/shortUrls", function (req, res) {
 app.get("/", (req, res) => {
 	res.render("index");
 });
-
-//const mySchema = new Schema({
-// _id: {
-// 	type: String,
-// 	default: () => nanoid()
-//   }
-// })
