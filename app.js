e9363099f9badf5834aa640bbead749f83843182
Jayvardhan Rathi <39644109+ComputerScientist-01@users.noreply.github.com>
2022-01-14 23:29:12 +0530

    ⚰️ remove console.log from production

--- app.js
+++ app.js
@@ -45,10 +45,5 @@ app.get("/:shortUrl", async (req, res) => {
 	res.redirect(shortUrl.full);
 });
 
-app.listen(process.env.PORT || 3000, function () {
-	console.log(
-		"Express server listening on port %d in %s mode",
-		this.address().port,
-		app.settings.env
-	);
-});
+app.listen(process.env.PORT || 3000);
+
