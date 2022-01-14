51ef63ddd93f9c3308eddf1113516558be2ae522
Jayvardhan Rathi <39644109+ComputerScientist-01@users.noreply.github.com>
2022-01-14 23:21:17 +0530

    📝 update documentation

--- db/connect.js
+++ db/connect.js
@@ -1,5 +1,10 @@
 const mongoose = require("mongoose");
 
+/**
+ * Connect to the database.
+ * @param url - The URL of the database to connect to.
+ * @returns The mongoose connection object.
+ */
 const connectDB = (url) => {
 	return mongoose.connect(url);
 };
