79ff608005c4d8b28c4c6e7a4b41e349b6dc3726
ComputerScientist-01 <rathi406@gmail.com>
2022-01-14 19:03:35 +0530

    💡Comments added

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 const port = 3000;
 
+// start mongo db connection
 const start = async () => {
 	try {
 		await connectDB(process.env.MONGO_URL);
@@ -19,11 +20,17 @@ const start = async () => {
 
 start();
 
+/**
+ * When the user visits the root URL, render the index template.
+ */
 app.get("/", async (req, res) => {
 	const shortUrls = await ShortUrl.find();
 	res.render("index", { shortUrls: shortUrls });
 });
 
+/*
+ * Create a new short URL.
+ */
 app.post("/shortUrls", async (req, res) => {
 	await ShortUrl.create({ full: req.body.fullUrl });
 
