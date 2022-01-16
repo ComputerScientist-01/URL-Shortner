3fe54be759aa22b76bf001be54fbe35de3d69f43
sandip2224 <sandipan2224@gmail.com>
2022-01-16 09:51:32 +0530

    Removed console logger for server status in app.js

--- app.js
+++ app.js
@@ -20,4 +20,4 @@ start();
 
 app.use('/', require('./routes/urlRoute'));
 
-app.listen(process.env.PORT || 3000, console.log(`Server running on port ${process.env.PORT || 3000}`));
+app.listen(process.env.PORT || 3000);
